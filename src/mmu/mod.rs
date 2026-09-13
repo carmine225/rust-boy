51d07b8bd2ec4e6d17639744b309195475e304b1
carmine225 <carmine.donnarumma.a@gmail.com>
2026-09-13 20:12:08 +0200

    fix: mmu memory read and write

--- src/mmu/mod.rs
+++ src/mmu/mod.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
-pub mod banking;
-pub mod bios;
-pub mod game;
+mod banking;
+mod bios;
+mod game;
+mod save;
 
 pub struct Mmu {
     wram: [u8; 8192],
@@ -12,11 +13,13 @@ pub struct Mmu {
     oam: [u8; 160],
     current_rom_bank: u8,
     current_ram_bank: u8,
+    ram_enabled: bool,
     card_rom: Vec<u8>,
     card_ram: Vec<u8>,
     game_path: PathBuf,
     bios: Vec<u8>,
     bios_path: PathBuf,
+    save_path: PathBuf,
 }
 
 impl Mmu {
@@ -29,19 +32,21 @@ impl Mmu {
             vram: [0; 8192],
             oam: [0; 160],
             current_rom_bank: 1,
-            current_ram_bank: 255,
+            current_ram_bank: 0,
+            ram_enabled: false,
             card_rom: Vec::new(),
             card_ram: Vec::new(),
             game_path: PathBuf::new(),
             bios: Vec::new(),
             bios_path: PathBuf::new(),
+            save_path: PathBuf::new(),
         }
     }
 
     pub fn read_byte(&self, address: u16) -> u8 {
         match address {
-            // ROM Cartuccia (0x0000 - 0x7FFF)
-            0x0000..=0x7FFF => {
+            // ROM Bank 00 (0x0000 - 0x3FFF) -> Primi 16 KiB fissi
+            0x0000..=0x3FFF => {
                 let idx = address as usize;
                 if idx < self.card_rom.len() {
                     self.card_rom[idx]
@@ -49,74 +54,88 @@ impl Mmu {
                     0xFF
                 }
             }
-            // VRAM (0x8000 - 0x9FFF)
-            0x8000..=0x9FFF => self.vram[(address - 0x8000) as usize],
 
-            // External RAM Cartuccia (0xA000 - 0xBFFF)
-            0xA000..=0xBFFF => {
-                let idx = (address - 0xA000) as usize;
-                if idx < self.card_ram.len() {
-                    self.card_ram[idx]
+            // ROM Bank 01..N (0x4000 - 0x7FFF) -> Calcolato col banco attivo
+            0x4000..=0x7FFF => {
+                let offset = ((self.current_rom_bank as usize * 0x4000)
+                    + ((address - 0x4000) as usize)) as usize;
+                if offset < self.card_rom.len() {
+                    self.card_rom[offset]
                 } else {
                     0xFF
                 }
             }
+
+            // VRAM (0x8000 - 0x9FFF)
+            0x8000..=0x9FFF => self.vram[(address - 0x8000) as usize],
+
+            // External RAM Cartuccia (0xA000 - 0xBFFF) -> Richiede RAM abilitata
+            0xA000..=0xBFFF => {
+                if !self.ram_enabled || self.card_ram.is_empty() {
+                    return 0xFF;
+                }
+                let offset =
+                    (self.current_ram_bank as usize * 0x2000) + (address - 0xA000) as usize;
+                if offset < self.card_ram.len() {
+                    self.card_ram[offset]
+                } else {
+                    0xFF
+                }
+            }
+
             // WRAM (0xC000 - 0xDFFF)
             0xC000..=0xDFFF => self.wram[(address - 0xC000) as usize],
 
-            // Echo RAM (0xE000 - 0xFDFF) -> specchio di WRAM
+            // Echo RAM (0xE000 - 0xFDFF)
             0xE000..=0xFDFF => self.wram[(address - 0xE000) as usize],
 
             // OAM (0xFE00 - 0xFE9F)
             0xFE00..=0xFE9F => self.oam[(address - 0xFE00) as usize],
 
-            // Area non utilizzata (0xFEA0 - 0xFEFF)
-            0xFEA0..=0xFEFF => 0x00,
+            // Area non utilizzata
+            0xFEA0..=0xFEFF => 0xFF,
 
-            // Registri I/O (0xFF00 - 0xFF7F)
+            // Registri I/O, HRAM, IE
             0xFF00..=0xFF7F => self.io_registers[(address - 0xFF00) as usize],
-
-            // HRAM (0xFF80 - 0xFFFE)
             0xFF80..=0xFFFE => self.hram[(address - 0xFF80) as usize],
-
-            // Interrupt Enable (0xFFFF)
             0xFFFF => self.ie_register,
         }
     }
 
     pub fn write_byte(&mut self, address: u16, value: u8) {
         match address {
-            // Scrittura in ROM -> usata dai chip MBC per cambiare banchi
-            0x0000..=0x7FFF => {}
-            // VRAM (0x8000 - 0x9FFF)
+            // Scrittura in ROM -> Inoltrata al gestore del modulo banking
+            0x0000..=0x7FFF => self.handle_mbc_write(address, value),
+
+            // VRAM
             0x8000..=0x9FFF => self.vram[(address - 0x8000) as usize] = value,
 
             // External RAM Cartuccia (0xA000 - 0xBFFF)
             0xA000..=0xBFFF => {
-                let idx = (address - 0xA000) as usize;
-                if idx < self.card_ram.len() {
-                    self.card_ram[idx] = value;
+                if self.ram_enabled && !self.card_ram.is_empty() {
+                    let offset =
+                        (self.current_ram_bank as usize * 0x2000) + (address - 0xA000) as usize;
+                    if offset < self.card_ram.len() {
+                        self.card_ram[offset] = value;
+                    }
                 }
             }
-            // WRAM (0xC000 - 0xDFFF)
+
+            // WRAM
             0xC000..=0xDFFF => self.wram[(address - 0xC000) as usize] = value,
 
-            // Echo RAM (0xE000 - 0xFDFF) -> scrive direttamente in WRAM
+            // Echo RAM
             0xE000..=0xFDFF => self.wram[(address - 0xE000) as usize] = value,
 
-            // OAM (0xFE00 - 0xFE9F)
+            // OAM
             0xFE00..=0xFE9F => self.oam[(address - 0xFE00) as usize] = value,
 
-            // Area non utilizzata (0xFEA0 - 0xFEFF)
+            // Area non utilizzata
             0xFEA0..=0xFEFF => {}
 
-            // Registri I/O (0xFF00 - 0xFF7F)
+            // Registri I/O, HRAM, IE
             0xFF00..=0xFF7F => self.io_registers[(address - 0xFF00) as usize] = value,
-
-            // HRAM (0xFF80 - 0xFFFE)
             0xFF80..=0xFFFE => self.hram[(address - 0xFF80) as usize] = value,
-
-            // Interrupt Enable (0xFFFF)
             0xFFFF => self.ie_register = value,
         }
     }
