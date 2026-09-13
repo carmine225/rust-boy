005b122c009f8945da75738336c3d598730f3a13
carmine225 <carmine.donnarumma.a@gmail.com>
2026-09-13 20:14:10 +0200

    fix: game and save manager

--- src/mmu/game.rs
+++ src/mmu/game.rs
@@ -5,6 +5,8 @@ use std::path::PathBuf;
 impl Mmu {
     pub fn load_game(&mut self, game_path: PathBuf) {
         self.card_rom = fs::read(game_path).expect("Failed to read game file");
+        self.manager(self.card_rom[0x147]);
+        self.ram_manager(self.card_ram[0x148]);
     }
 
     pub fn get_game_path(&mut self) {
--- src/mmu/save.rs
+++ src/mmu/save.rs
@@ -1,5 +1,29 @@
 use crate::mmu::Mmu;
+use std::fs;
+use std::io::{Read, Write};
+use std::path::PathBuf;
 
 impl Mmu {
-    // save game data to a file (create saves)
+    // Carica il file .sav all'avvio
+    pub fn load_save_file(&mut self, save_path: &str) {
+        if let Ok(mut file) = fs::File::open(save_path) {
+            let mut buffer = Vec::new();
+            if file.read_to_end(&mut buffer).is_ok() {
+                self.card_ram = buffer;
+            }
+        }
+    }
+
+    // Salva il vettore card_ram su file .sav
+    pub fn save_to_disk(&self, save_path: &str) {
+        if !self.card_ram.is_empty() {
+            if let Ok(mut file) = fs::File::create(save_path) {
+                let _ = file.write_all(&self.card_ram);
+            }
+        }
+    }
+    pub fn get_save_path(&mut self) {
+        fs::create_dir_all("saves").unwrap();
+        self.save_path = PathBuf::from("saves");
+    }
 }
